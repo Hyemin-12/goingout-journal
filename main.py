5f305ab8464d1f033d78febe649b87e03cc97c70
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-10-18 09:30:07 +0900

    명단 확인 및 학생 찾기

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+global selected
+
+roomList = [
+    {101 : ["미림1", "미림2", "미림3"]},
+    {102 : ["미림4", "미림5", "미림6"]},
+    {103 : ["미림7", "미림8", "미림9"]},
+    {104 : ["미림10", "미림11", "미림12"]}
+]
+
+def showList():
+    for i in range(0, 4):
+        for k, v in roomList[i].items():
+            print(f"{k} : ", end="")
+            for i in range(0, 3):
+                print(v[i], end=" ")
+            print()
+
+def findRoom(roomNum):
+    for i in range(0, 4):
+        for k, v in roomList[i].items():
+            if(k == roomNum):
+                selected = roomList[i].get(roomNum)
+                print(f"{k} : ", end="")
+                for i in range(0, 3):
+                    print(v[i], end=" ")
+                print()
+
+def findStudent(studentName):
+    for i in range(0, 3):
+        if(selected[i] == studentName):
+            print(f"{selected[i]} 학생의 상태를 설정합니다.")
+
+            print("="*30)
+            print("1. 잔류")
+            print("2. 외출")
+            print("3. 외박")
+            print("="*30)
+            selectState = int(input("상태를 지정해주세요 : "))
+
+            return selectState
+
+            break
+
 while True:
     print("="*30)
     print("0. 종료")
@@ -11,10 +54,15 @@ while True:
         print("프로그램을 종료합니다.")
         break
     elif select == "1":
-        print("'명단")
+        print("명단")
+        showList()
         break
     elif select == "2":
         print("설정")
+        selectRoom = int(input("상태를 설정하려는 학생의 호실 번호를 입력하세요 : "))
+        findRoom(selectRoom)
+        selectStudent = input("상태를 설정하려는 학생의 이름을 입력해주세요 : ")
+        state = findStudent(selectStudent)
         break
     elif select == "3":
         print("업데이트")
