a5f48ddb1aa9020718bc691ecd0802191a30da15
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-10-19 22:44:54 +0900

    상태 기본값 설정

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ roomList = [
     {104 : ["미림10", "미림11", "미림12"]}
 ]
 
+global states
+states = ["잔류중", "외박중", "외출중"]
+
 def showList():
     for i in range(0, 4):
         for k, v in roomList[i].items():
@@ -23,11 +26,17 @@ def findRoom(roomNum):
                 for i in range(0, 3):
                     print(v[i], end=" ")
                 print()
+                print()
+                return True
+                break
+            else:
+                return False
 
-def findStudent(studentName):
+def findStudent(studentName, state = states[0]):
     for i in range(0, 3):
         if(selected[i] == studentName):
-            print(f"{selected[i]} 학생의 상태를 설정합니다.")
+            print(f"{selected[i]} 학생의 상태 : {state}")
+            print()
             break
 
 while True:
@@ -45,29 +54,36 @@ while True:
     elif select == 1:
         showList()
     elif select == 2:
-        print()
-        selectRoom = int(input("상태를 확인하려는 학생의 호실 번호를 입력하세요 : "))
-        findRoom(selectRoom)
-        selectStudent = input("상태를 확인하려는 학생의 이름을 입력해주세요 : ")
-
         while True:
             print()
-            print(f"{selectStudent} 학생의 상태를 설정합니다.")
-            print("-"*30)
-            print("1. 잔류 / 외박 / 외출 상태 변경")
-            print("2. 나가기")
-            print("-"*30)
-            select2 = int(input("할 일을 선택하세요 : "))
-            print()
+            selectRoom = int(input("상태를 확인하려는 학생의 호실 번호를 입력하세요 : "))
 
-            if select2 == 1:
-                print("상태 변경")
-                break
-            elif select2 == 2:
-                print("나가기")
+            if findRoom(selectRoom):
+                selectStudent = input("상태를 확인하려는 학생의 이름을 입력해주세요 : ")
+
+                while True:
+                    print()
+                    findStudent(selectStudent)
+                    print("-"*30)
+                    print("1. 잔류 / 외박 / 외출 상태 변경")
+                    print("2. 나가기")
+                    print("-"*30)
+                    select2 = int(input("할 일을 선택하세요 : "))
+                    print()
+
+                    if select2 == 1:
+                        print("상태 변경")
+                        
+                        break
+                    elif select2 == 2:
+                        print("나가기")
+                        break
+                    else:
+                        print("잘못된 메뉴입니다. 다시 입력해주세요.")
+                        continue
                 break
             else:
-                print("잘못된 메뉴입니다. 다시 입력해주세요.")
+                print("잘못된 호실 번호입니다. 다시 입력해주세요.")
                 continue
 
     else:
