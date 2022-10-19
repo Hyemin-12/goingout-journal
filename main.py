1c689a42e22fc539cdec77bf227a423b2d2396fc
xodus <s2121@e-mirim.hs.kr>
2022-10-20 08:48:24 +0900

    comment 및 필요 없는 부분 삭제

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# 기숙사생 명단
 roomList = [
     {101 : ["미림1", "미림2", "미림3"]},
     {102 : ["미림4", "미림5", "미림6"]},
@@ -5,17 +6,21 @@ roomList = [
     {104 : ["미림10", "미림11", "미림12"]}
 ]
 
+# 잔류 / 외박 / 외출 상태 리스트
 global states
 states = ["잔류중", "외박중", "외출중"]
 
+# 명단 확인 함수
 def showList():
     for i in range(0, 4):
         for k, v in roomList[i].items():
             print(f"{k} : ", end="")
             for i in range(0, 3):
                 print(v[i], end=" ")
-            print()
+            print()   
+    print()
 
+# 호실 찾는 함수
 def findRoom(roomNum):
     for i in range(0, 4):
         for k, v in roomList[i].items():
@@ -26,12 +31,8 @@ def findRoom(roomNum):
                 for i in range(0, 3):
                     print(v[i], end=" ")
                 print()
-                print()
-                return True
-                break
-            else:
-                return False
 
+# 학생 찾는 함수
 def findStudent(studentName, state = states[0]):
     for i in range(0, 3):
         if(selected[i] == studentName):
@@ -39,6 +40,7 @@ def findStudent(studentName, state = states[0]):
             print()
             break
 
+# 메인 메뉴
 while True:
     print("="*30)
     print("0. 종료")
@@ -48,44 +50,34 @@ while True:
     select = int(input("할 일을 선택하세요 : "))
     print()
 
-    if select == 0:
+    if select == 0: # 프로그램 종료
         print("프로그램을 종료합니다.")
         break
-    elif select == 1:
+    elif select == 1: # 기숙사생 명단 확인
         showList()
-    elif select == 2:
+    elif select == 2: # 특정 기숙사생 상태 확인
+        print()
+        selectRoom = int(input("상태를 확인하려는 학생의 호실을 입력하세요 : "))
+        findRoom(selectRoom)
+        selectStudent = input("상태를 확인하려는 학생의 이름을 입력해주세요 : ")
         while True:
             print()
-            selectRoom = int(input("상태를 확인하려는 학생의 호실 번호를 입력하세요 : "))
-
-            if findRoom(selectRoom):
-                selectStudent = input("상태를 확인하려는 학생의 이름을 입력해주세요 : ")
-
-                while True:
-                    print()
-                    findStudent(selectStudent)
-                    print("-"*30)
-                    print("1. 잔류 / 외박 / 외출 상태 변경")
-                    print("2. 나가기")
-                    print("-"*30)
-                    select2 = int(input("할 일을 선택하세요 : "))
-                    print()
-
-                    if select2 == 1:
-                        print("상태 변경")
-                        
-                        break
-                    elif select2 == 2:
-                        print("나가기")
-                        break
-                    else:
-                        print("잘못된 메뉴입니다. 다시 입력해주세요.")
-                        continue
+            findStudent(selectStudent)
+            print("-"*30)
+            print("1. 잔류 / 외박 / 외출 상태 변경")
+            print("2. 나가기")
+            print("-"*30)
+            select2 = int(input("할 일을 선택하세요 : "))
+            print()
+            if select2 == 1: # 잔류 / 외박 / 외출 상태 변경
+                print("상태 변경")
+                break
+            elif select2 == 2: # 메뉴 나가기
+                print("나가기")
                 break
             else:
-                print("잘못된 호실 번호입니다. 다시 입력해주세요.")
+                print("잘못된 메뉴입니다. 다시 입력해주세요.")
                 continue
-
     else:
         print("없는 메뉴입니다. 다시 입력해주세요.")
         continue
