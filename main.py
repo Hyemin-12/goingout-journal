23d2fe9914070a2095f564b7b9a2153064b10167
xodus <s2121@e-mirim.hs.kr>
2022-10-20 09:40:00 +0900

    날짜 별 상태 확인 기능 추가

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ while True:
     print("0. 종료")
     print("1. 기숙사생 명단 확인")
     print("2. 특정 기숙사생 상태 확인")
+    print("3. 기숙사생 추가 / 삭제")
+    print("4. 날짜 별 기숙사생 상태 확인")
     print("="*30)
     select = int(input("할 일을 선택하세요 : "))
     print()
@@ -76,8 +78,31 @@ while True:
                 print("나가기")
                 break
             else:
-                print("잘못된 메뉴입니다. 다시 입력해주세요.")
+                print("존재하지 않는 메뉴입니다. 다시 입력해주세요.")
                 continue
+    elif select == 3:
+        while True:
+            print("-"*30)
+            print("1. 기숙사생 추가")
+            print("2. 기숙사생 삭제")
+            print("3. 나가기")
+            print("-"*30)
+            select3 = int(input("할 일을 선택하세요 : "))
+            print()
+            if select3 == 1: # 기숙사생 추가
+                print("기숙사생 추가")
+                break
+            elif select3 == 2:
+                print("기숙사생 삭제")
+                break
+            elif select3 == 3: # 메뉴 나가기
+                print("나가기")
+                break
+            else:
+                print("존재하지 않는 메뉴입니다. 다시 입력해주세요.")
+                continue
+    elif select == 4:
+        print("날짜 별 기숙사생 상태 확인")
     else:
-        print("없는 메뉴입니다. 다시 입력해주세요.")
+        print("존재하지 않는 메뉴입니다. 다시 입력해주세요.")
         continue
