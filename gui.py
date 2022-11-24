13710511d95f15c3a4009b201dab48876cb8890c
hyennin <chm050202@gmail.com>
2022-11-24 13:25:43 +0900

    qtdesigner 사용, 버튼 클릭 시 창 전환

--- gui.py
+++ gui.py
@@ -1,60 +1,45 @@
+import os
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QDesktopWidget, QHBoxLayout, QVBoxLayout
-from PyQt5.QtCore import Qt
 
-class MyApp(QWidget):
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
+def resource_path(relative_path):
+    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+form = resource_path('main.ui')
+form_class = uic.loadUiType(form)[0]
+
+form_second = resource_path('second.ui')
+form_secondWindow = uic.loadUiType(form_second)[0]
+
+
+class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
 
-    def initUI(self):
-        title = QLabel('외출일지', self)
-        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    def btn_main_to_second(self):
+        self.hide()                     # 메인윈도우 숨김
+        self.second = secondWindow()    #
+        self.second.exec()              # 두번째 창을 닫을 때 까지 기다림
+        self.show()                     # 두번째 창을 닫으면 다시 첫 번째 창이 보여짐짐
 
-        titleFont = title.font()
-        titleFont.setPointSize(20)
-        title.setFont(titleFont)
-
-        btn1 = QPushButton('기숙사생 명단 확인')
-        btn2 = QPushButton('특정 기숙사생 상태 확인')
-        btn3 = QPushButton('기숙사생 추가 / 삭제')
-        btn4 = QPushButton('날짜 별 기숙사생 상태 확인')
-        okButton = QPushButton('OK')
-        cancelButton = QPushButton('Cancel')
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(okButton)
-        hbox.addWidget(cancelButton)
-        hbox.addStretch(1)
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addWidget(title)
-        vbox.addStretch(1)
-        vbox.addWidget(btn1)
-        vbox.addWidget(btn2)
-        vbox.addWidget(btn3)
-        vbox.addWidget(btn4)
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
-
-        self.setLayout(vbox)
-
-        self.setWindowTitle('외출일지')
-        self.resize(800, 600)
-        self.center()
+class secondWindow(QDialog,QWidget,form_secondWindow):
+    def __init__(self):
+        super(secondWindow, self).__init__()
+        self.initUi()
         self.show()
 
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def initUi(self):
+        self.setupUi(self)
+
+    def btn_second_to_main(self):
+        self.close()                    #클릭시 종료됨.
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
